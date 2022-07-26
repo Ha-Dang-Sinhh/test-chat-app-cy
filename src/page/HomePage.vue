21e25a08d31636c9b08f527ca1925faaab2cb4a5
Sinh <hadangsinh1911@gmail.com>
2022-07-26 16:44:45 +0700

    add code

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto ">
     <div class="w-4/6 mx-auto border mt-20 rounded lg:grid lg:grid-cols-3">
       <div class="lg:col-span-3 lg:block">
-        <form @submit.prevent="" class="w-full">
+        <div class="w-full">
           <div class="w-full relative flex items-center p-3 border-b border-gray-300">
             <img class="object-cover w-10 h-10 rounded-full"
                  src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg" alt="username" />
@@ -21,7 +21,7 @@
                   <div v-if="mes.message !== ''" :class="user.uid == mes.uid ? 'relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow flex justify-end':'relative max-w-xl px-4 py-2 text-gray-700 rounded shadow'">
                     <span class="block">{{mes.message}}</span>
                   </div>
-                  <div v-if="mes.urls.length > 0" class="mt-1 flex justify-end">
+                  <div v-if="mes.urls.length > 0" :class="user.uid == mes.uid ? 'mt-1 flex justify-end' : 'mt-1 flex justify-start'">
                     <img class="w-40 h-40 object-cover overflow-hidden" :src="mes.urls[0]" alt="">
                   </div>
                 </div>
@@ -34,7 +34,7 @@
               <i class="w-6 h-6 absolute z-10 top-2 left-32 rounded-full text-center flex items-center justify-center cursor-pointer opacity-30 hover:opacity-100 hover:bg-gray-200 bg-gray-300 text-2xl" @click="removeSelectedImage(index)"><i class="fa-solid fa-xmark"></i></i>
             </div>
           </div>
-          <div class="flex items-center justify-between w-full p-3 border-t border-gray-300 ">
+          <form @submit.prevent="" class="flex items-center justify-between w-full p-3 border-t border-gray-300 ">
             <label for="image" class="cursor-pointer text-gray-500">
               <i class="fa-solid fa-image"></i>
               <input type="file" id="image" class="hidden" access="image/*" @change="imageChange($event)" multiple>
@@ -50,8 +50,8 @@
                     d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
               </svg>
             </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   </div>
